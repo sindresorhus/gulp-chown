b88a35a7bdc381efc71764face7d95f3201f90cc
Sindre Sorhus <sindresorhus@gmail.com>
2026-02-02 11:22:39 +0700

    Add failing test for gulp.dest() not applying chown
    Fixes #5

--- test.js
+++ test.js
@@ -21,6 +21,37 @@ test('chown files', async t => {
 	t.is(file.stat.gid, 20);
 });
 
+test.failing('chown is applied to file on disk when using gulp.dest() (gh-5)', async t => {
+	const {default: gulp} = await import('gulp');
+	const {mkdtemp, stat, writeFile, rm} = await import('node:fs/promises');
+	const {tmpdir} = await import('node:os');
+	const {join} = await import('node:path');
+
+	const sourceDirectory = await mkdtemp(join(tmpdir(), 'gulp-chown-src-'));
+	const destinationDirectory = await mkdtemp(join(tmpdir(), 'gulp-chown-dest-'));
+	const sourceFile = join(sourceDirectory, 'test.txt');
+	await writeFile(sourceFile, 'hello');
+
+	const targetUid = 99;
+	const targetGid = 99;
+
+	await new Promise((resolve, reject) => {
+		gulp.src(sourceFile)
+			.pipe(chown(targetUid, targetGid))
+			.pipe(gulp.dest(destinationDirectory))
+			.on('end', resolve)
+			.on('error', reject);
+	});
+
+	const destinationFile = join(destinationDirectory, 'test.txt');
+	const destinationStat = await stat(destinationFile);
+	t.is(destinationStat.uid, targetUid);
+	t.is(destinationStat.gid, targetGid);
+
+	await rm(sourceDirectory, {recursive: true});
+	await rm(destinationDirectory, {recursive: true});
+});
+
 if (!('CI' in process.env)) {
 	test.failing('chown files using a username', async t => {
 		const username = 'root';
